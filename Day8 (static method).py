d1a4b94b4b9caf34799011c9fc6b2a980e07fb97
Jackson <56949000+jacksonchen1998@users.noreply.github.com>
2022-04-12 23:15:12 +0800

    new

---
+++ Day8 (static method).py
@@ -0,0 +1,37 @@
+from math import sqrt
+
+class Triangle(object):
+
+    def __init__(self, a, b, c):
+        self._a = a
+        self._b = b
+        self._c = c
+
+    @staticmethod
+    def is_valid(a, b, c):
+        return a + b > c and b + c > a and a + c > b
+
+    def perimeter(self):
+        return self._a + self._b + self._c
+
+    def area(self):
+        half = self.perimeter() / 2
+        return sqrt(half * (half - self._a) * (half - self._b) * (half - self._c))
+
+def main():
+    a = int(input("a : "))
+    b = int(input("b : "))
+    c = int(input("c : "))
+
+    # Set class
+    if Triangle.is_valid(a, b, c):
+        t = Triangle(a, b, c)
+        print(t.perimeter())
+        # print(Triangle.perimeter(t))
+
+        print(t.area())
+        # print(Triangle.area(t))
+    else:
+        print("Can't be trinagle !!!")
+
+main()
