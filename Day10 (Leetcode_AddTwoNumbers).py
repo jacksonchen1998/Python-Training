ab083c6402a4f0b320d358afed19074b445b2ff4
Jackson <56949000+jacksonchen1998@users.noreply.github.com>
2022-04-22 20:29:53 -0700

    new

---
+++ Day10 (Leetcode_AddTwoNumbers).py
@@ -0,0 +1,50 @@
+l1 = [3, 4, 5]
+l2 = [5, 6, 7]
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+        l1 = ListNode()
+        l2 = ListNode()
+        
+        carry = 0
+        dummy = ListNode(0)
+        p = dummy
+        
+        while l1 and l2:
+            p.next = ListNode((l1.val + l2.val + carry) % 10)
+            carry = (l1.val + l2.val + carry) // 10
+            l1 = l1.next
+            l2 = l2.next
+            p = p.next
+            
+        if l1:
+            while l1:
+                p.next = ListNode((l1.val + carry) % 10)
+                carry = (l1.val + carry) // 10
+                l1 = l1.next
+                p = p.next
+        if l2:
+            while l2:
+                p.next = ListNode((l2.val + carry) % 10)
+                carry = (l2.val + carry) // 10
+                l2 = l2.next
+                p = p.next
+                
+        if carry == 1:
+            p.next = ListNode(1)
+            
+        return dummy.next
+
+s = Solution()
+print(s.addTwoNumbers(l1, l2))
